3b09c02e68d67d4642152bf782a001fb089f9a94
Shane-S <shane.d.spoor@gmail.com>
2013-12-02 08:51:10 -0800

    Added comments to OpenDialog.cpp

--- OpenDialog.cpp
+++ OpenDialog.cpp
@@ -1,8 +1,48 @@
+/*------------------------------------------------------------------------------------------------------------------
+-- SOURCE FILE:	OpenDialog.cpp		A collection of functions for displaying output to the window (packet statistics
+--									and received data).
+--
+-- PROGRAM:		BCP
+--
+-- FUNCTIONS:
+--	void FileInitialize(HWND hwnd, OPENFILNAME *ofn);
+--	BOOL FileOpenDlg(HWND hwnd, PSTR filename, OPENFILENAME openfilename);
+--
+-- DATE: 		November 26, 2013
+--
+-- REVISIONS: 	none
+--
+-- DESIGNER: 	Ashley Tham
+--
+-- PROGRAMMERS: Ashley Tham
+--
+-- NOTES:
+-- All functions return a boolean success value.
+----------------------------------------------------------------------------------------------------------------------*/
+
 #include "BCP.h"
-#include <commdlg.h> //might need this
-//static OPENFILENAME ofn ; //do need this
+#include <commdlg.h>
 
-
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION: FileInitialize
+--
+-- DATE: November 30, 2013
+--
+-- REVISIONS: None
+--
+-- DESIGNER: Ashley Tham
+--
+-- PROGRAMMER: Ashley Tham
+--
+-- INTERFACE: FileInitialize(HWND hwnd, OPENFILENAME *ofn)
+-					HWND hwnd: Handle to the parent window.
+--					OPENFILENAME *ofn: Points to a struct containing necessary info for opening a file.
+--
+-- RETURNS: void;
+--
+-- NOTES:
+-- Sets up the OPENFILENAME structure to allow users to open files.
+----------------------------------------------------------------------------------------------------------------------*/
 void FileInitialize (HWND hwnd, OPENFILENAME *ofn)
 {
 	static TCHAR szFilter[] = TEXT ("Text Files (*.TXT)\0*.txt\0")  
@@ -32,6 +72,27 @@ void FileInitialize (HWND hwnd, OPENFILENAME *ofn)
 	ofn->Flags             = OFN_HIDEREADONLY | OFN_CREATEPROMPT ;
 }
 
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION: FileOpenDlg
+--
+-- DATE: November 30, 2013
+--
+-- REVISIONS: None
+--
+-- DESIGNER: Ashley Tham
+--
+-- PROGRAMMER: Ashley Tham
+--
+-- INTERFACE: FileOpenDlg(HWND hwnd, PSTR pstrFileName OPENFILENAME *ofn)
+--					HWND hwnd:			Handle to the parent window.
+--					PSTR pstrFileName:	Name of the file to open
+--					OPENFILENAME *ofn:	Points to a struct containing necessary info for opening a file.
+--
+-- RETURNS: void;
+--
+-- NOTES:
+-- Sets up the OPENFILENAME structure to allow users to open files.
+----------------------------------------------------------------------------------------------------------------------*/
 BOOL FileOpenDlg (HWND hwnd, PTSTR pstrFileName, OPENFILENAME *ofn)
 {
 	ofn->hwndOwner         = hwnd ;
