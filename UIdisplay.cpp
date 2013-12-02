afbc4e58cfcdf5beee17a4d195f0263536279ae9
Cholisky <Cholisky@gmail.com>
2013-12-01 16:11:25 -0800

    Text seems to be appending in edit box

--- UIdisplay.cpp
+++ UIdisplay.cpp
@@ -18,39 +18,56 @@ VOID SetupGUI(HWND main, HWND edit)
 -- DATE: November 19, 2013
 --
 -- REVISIONS: (Date and Description)
+--			Dec 1, 2013
+--			Chris Holisky
+--			Changed function to take byte instead of char
+--			Changed funtion to take only one argument
 --
 -- DESIGNER: Chris Holisky
 --
 -- PROGRAMMER: Chris Holisky
 --
--- INTERFACE: VOID GUI_Text(LPCSTR text)
--- LPCSTR text : Pointer to the text to be written to screen
+-- INTERFACE: VOID GUI_Text(TCHAR* text)
+-- TCHAR* text : Text to be written on screen
 --
 -- RETURNS: void;
 -- 
 -- NOTES:
--- This function is used to write data to the edit box.  It appends new datato the currently existing text.
+-- This function is used to write data to the edit box.  It appends new data to the currently existing text.
 --
 ----------------------------------------------------------------------------------------------------------------------*/
 VOID GUI_Text(TCHAR* text)
 {
+	//text=(TCHAR*)"This is a test.  This is only a test.  Please do not be alarmed.  If it works....";
 	//This might be what we have to use.  We'd have to create a buffer to 
 	//hold all text and then append text to buffer and then redisplay it
 	//SetWindowText(hwndTextBox, text);
 	/*
 	//Put window text in string buffer;
 	int len = GetWindowTextLength(hwndTextBox);
-	wchar_t * scrBuffer;
+	wchar_t * scrBuffer[10000000];
 	wchar_t buffer[10000];
-	GetWindowText(hwndTextBox, scrBuffer, len+1);
+	GetWindowText(hwndTextBox, (LPWSTR)scrBuffer, len+1);
 	(scrBuffer, sizeof(text), text);
 	//Concatenate new text to window text
 	*/
 	//put total back on screen
 
-
-
-	SetWindowText(hwndTextBox, text);
+ //HWND hEdit = GetDlgItem (,);
+   int ndx = GetWindowTextLength (hwndTextBox);
+   SetFocus (hwndTextBox);
+   #ifdef WIN32
+      SendMessage (hwndTextBox, EM_SETSEL, (WPARAM)ndx, (LPARAM)ndx);
+   #else
+      SendMessage (hwndTextBox, EM_SETSEL, 0, MAKELONG (ndx, ndx));
+   #endif
+	     #ifdef WIN32
+      SendMessage (hwndTextBox, EM_SETSEL, (WPARAM)ndx, (LPARAM)ndx);
+   #else
+      SendMessage (hwndTextBox, EM_SETSEL, 0, MAKELONG (ndx, ndx));
+   #endif
+      SendMessage (hwndTextBox, EM_REPLACESEL, 0, (LPARAM) ((LPSTR) text));
+	//SetWindowText(hwndTextBox, (LPCWSTR)NULL);
 	/*
 	int len = GetWindowTextLength(hwndTextBox);
 	std::vector<TCHAR> temp(len + sizeof(text) + 1);
